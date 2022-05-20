6bbbc488e2c05669964e3119a72faf9ebaf2da0e
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-05-20 22:32:57 +0100

    updated routes

--- 03-Client-Portal/src/App.js
+++ 03-Client-Portal/src/App.js
@@ -52,7 +52,13 @@ function App() {
               <Route path="/calendar" element="Calendar" />
               <Route path="/color-picker" element="ColorPicker" />
               {/* Charts */}
-              <Route path="/line" element="Calendar" />
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
         </div>
