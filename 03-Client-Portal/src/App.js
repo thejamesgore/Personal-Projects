6673675ad61e05e0329254c59dc477c948a39bf6
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-05-20 21:50:14 +0100

    added routes

--- 03-Client-Portal/src/App.js
+++ 03-Client-Portal/src/App.js
@@ -20,7 +20,26 @@ function App() {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div>Sidebar</div> : <div>Sidebar w-0</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-72' : 'flex-2'
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              NavBar
+            </div>
+          </div>
+
+          <div>
+            <Routes></Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
