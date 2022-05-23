051b8ba998a23912f492b0d27b000f958fd33106
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-05-23 09:56:55 +0100

    fixed navbar styling issue

--- 03-Client-Portal/src/components/Navbar.jsx
+++ 03-Client-Portal/src/components/Navbar.jsx
@@ -38,9 +38,8 @@ const Navbar = () => {
       style={{color}}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
       >
-        <span stlye={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+        <span stlye={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
         {icon}
-        </span>
       </button>
     </TooltipComponent>
   )
