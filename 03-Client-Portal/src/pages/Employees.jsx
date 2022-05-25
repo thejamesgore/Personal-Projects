e010f5c115e7776dde866f6b00cc697c54661d31
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-05-25 11:12:18 +0100

    added employee section

--- 03-Client-Portal/src/pages/Employees.jsx
+++ 03-Client-Portal/src/pages/Employees.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import {employeesData, contextMenuItems, employeesGrid} from '../data/dummy'
+import {Header} from '../components'
+
+
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        id='gridcomp'
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+
+      >
+        <ColumnsDirective >
+          {employeesGrid.map((item, index) =>(
+            <ColumnDirective key={index} {...item}/>))}
+        </ColumnsDirective>
+        <Inject 
+          services={[Search, Page, Edit]}
+        />
+      </GridComponent>
+    </div>
   )
 }
 
