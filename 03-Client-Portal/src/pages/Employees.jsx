2eb59f7d3d45c00761a99e5cc4e6e4f95b64f8fd
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-05-25 11:18:59 +0100

    added search to employees page

--- 03-Client-Portal/src/pages/Employees.jsx
+++ 03-Client-Portal/src/pages/Employees.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Edit, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
 import {employeesData, contextMenuItems, employeesGrid} from '../data/dummy'
 import {Header} from '../components'
 
@@ -10,18 +10,18 @@ const Employees = () => {
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header title="Employees" category="Page" />
       <GridComponent
-        id='gridcomp'
         dataSource={employeesData}
         allowPaging
         allowSorting
-
+        toolbar={['Search']}
+        width="auto"
       >
         <ColumnsDirective >
           {employeesGrid.map((item, index) =>(
             <ColumnDirective key={index} {...item}/>))}
         </ColumnsDirective>
         <Inject 
-          services={[Search, Page, Edit]}
+          services={[Search, Page, Toolbar]}
         />
       </GridComponent>
     </div>
