ed83af70644aee7d1f490f49e4b7e228684dfd19
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-06-01 13:42:38 +0100

    adding color picker

--- 03-Client-Portal/src/pages/ColorPicker.jsx
+++ 03-Client-Portal/src/pages/ColorPicker.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
+
+import { Header } from '../components'
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Piker" />ColorPicker</div>
   )
 }
 
