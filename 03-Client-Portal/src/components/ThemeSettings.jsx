073488eaf1a175e9455d34f4e368024ff3499614
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-06-01 15:00:10 +0100

    added light and dark theme options

--- 03-Client-Portal/src/components/ThemeSettings.jsx
+++ 03-Client-Portal/src/components/ThemeSettings.jsx
@@ -11,10 +11,41 @@ const ThemeSettings = () => {
     <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
       <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484b52] w-400'>
         <div className="flex justify-between items-center p-4 ml-4">
-          <p className='font-semibold text-lg'>Settings</p>
+          <p className='font-semibold text-xl'>Settings</p>
           <button className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray' type='button' onClick={() => {}} style={{ color: 'rgb(153,171,180', borderRadius: '50%'}}>
             <MdOutlineCancel/>
           </button>
+        </div>
+        <div className='flex-col border-t-1 border-color p-4 ml-4'>
+          <p className='font-semibold text-lg'>Theme Options</p>
+
+          <div className='mt-4'>
+            <input
+            type='radio'
+            id='light'
+            name='theme'
+            value='Light'
+            className='cursor-pointer'
+            onChange={() => {}}
+            checked={true}
+            />
+            <label htmlFor='light'
+            className='ml-2 text-md cursor-pointer'>Light</label>
+            </div> 
+          <div className='mt-4'>
+            <input
+            type='radio'
+            id='dark'
+            name='theme'
+            value='Dark'
+            className='cursor-pointer'
+            onChange={() => {}}
+            checked={true}
+            />
+            <label htmlFor='dark'
+            className='ml-2 text-md cursor-pointer'>Dark</label>
+            </div> 
+
         </div>
       </div>
     </div>
